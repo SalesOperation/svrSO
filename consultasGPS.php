981339470b31a859a2de6afae0d18b7263549676
SalesOperation <arodi.morga@tigo.com.hn>
2018-03-01 14:50:13 -0600

    Add files via upload

--- consultasGPS.php
+++ consultasGPS.php
@@ -2,26 +2,22 @@
 	header('Access-Control-Allow-Origin: *');
 	$url = 'http://iteshn.hol.es/server_app/svrConsultasSO.php';
 	// create curl resource 
-        try{
-             $ch = curl_init(); 
+    try{
+         $ch = curl_init(); 
 
         // set url 
         curl_setopt($ch, CURLOPT_URL, $url); 
-
         //return the transfer as a string 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
         // $output contains the output string 
         $output = curl_exec($ch); 
-        echo $output;
-
         // close curl resource to free up system resources 
         curl_close($ch);
-        }catch(Exception  $ex){
-               echo $ex;
-        }
-        
+        echo $output;
 
+    }catch(Exception  $ex){
+        echo $ex;
+    }
 ?>
 
 
